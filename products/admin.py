1269a44cb91fbfd12136237e3ca544e388560c78
Faranak Fallahi <fallahifaranak422@gmail.com>
2025-06-30 13:56:32 +0330

    add some changes in comments

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Comment
 
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ('author', 'body', 'recommend', 'active',)
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'active',  )
+
+    inlines = [
+        CommentsInline,
+
+    ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'body', 'recommend','product' ,'active', )
 
 
--- products/models.py
+++ products/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
+
+
+# custom manager
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
+
+# how to use custom manageR
+#         Comment.active_comments_manager.all()
+
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
@@ -15,3 +27,18 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
 
+
+class Comment(models.Model):
+    body = models.TextField(verbose_name='Please write your comment: ')
+    author = models.ForeignKey(
+
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Comment Author'
+    )
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    active = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True, verbose_name='Do you recommend this product?')
+    datetime_created = models.DateTimeField(auto_now_add=True)
